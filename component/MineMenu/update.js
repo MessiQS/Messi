febc07d321997ed820becd05a24c8f0d0f7347cf
mike <573940421@qq.com>
2017-06-14 23:46:43 +0800

    补交信息

---
+++ component/MineMenu/update.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Image,
+    // View,
+    // Text
+} from 'react-native';
+import {
+    Container,
+    Text,
+    Header,
+    Title,
+    Body,
+    View,
+    Thumbnail,
+    Content,
+    List,
+    ListItem,
+    Right,
+    Icon,
+    Button
+} from 'native-base';
+
+export default class Update extends Component {
+    static navigationOptions = ({ navigation }) => ({
+        title: `版本更新`,
+        tabBarVisible:false,
+
+    });
+    render() {
+        return (
+            <Container contentContainerStyle={{flex: 1}}>
+                <Content>
+                    <Text>
+                        版本更新
+                    </Text>
+                </Content>
+            </Container>
+        );
+    }
+}
+
+
